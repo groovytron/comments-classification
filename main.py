5ab29c2d7b4f80e710bdea598d022b756a61c00d
Yoan Blanc <yoan@dosimple.ch>
2017-03-27 12:19:33 +0200

    yield c'est bon.
    Du code Python idiomatique.

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import re
 
+PATTERN = re.compile('(ADV|NOM|VER|ADJ)')
+
 
 def preprocess_file(file_name):
-    words = []
-    PATTERN = re.compile('(ADV|NOM|VER|ADJ)')
-
     with open(file_name, 'r') as f:
         for line in f:
-            original_form, nature, canonical_form = tuple(line.split())
-            if PATTERN.match(nature) is not None:
-                words.append(canonical_form)
-                #print('original:', original_form, 'nature:', nature,
-                #      'canonical:', canonical_form)
-
-    f.closed
-    return words
+            _, nature, canonical_form = tuple(line.split(maxsplit=3))
+            if PATTERN.match(nature):
+                yield canonical_form
 
 
 if __name__ == '__main__':
-    file_canonical_words = preprocess_file('data/tagged/neg/neg-0000.txt')
+    file_canonical_words = tuple(preprocess_file('data/tagged/neg/neg-0000.txt'))
     print(file_canonical_words)
